47aa372f18d81acc873247b5a561f3214d09e508
Andreas Hiess <andreas.hiess@student.tugraz.at>
2020-03-21 15:01:51 +0100

    setup-project

---
+++ src/main/java/ASD/Morning4/controller/ExampleController.java
@@ -0,0 +1,18 @@
+package ASD.Morning4.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("")
+public class ExampleController {
+
+    @GetMapping("")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+}
---
+++ src/test/java/ASD/Morning4/Morning4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ASD.Morning4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Morning4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
