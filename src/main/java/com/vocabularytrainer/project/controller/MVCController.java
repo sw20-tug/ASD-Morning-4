8b0dc3e7c93f3ff6f851d6283990de04ee4b19b3
Simon Ranftl <simon.ranftl82@gmail.com>
2020-04-01 12:31:35 +0200

    LANG-003 Added Edit page [LJ, RS]

--- src/main/java/com/vocabularytrainer/project/controller/MVCController.java
+++ src/main/java/com/vocabularytrainer/project/controller/MVCController.java
@@ -11,7 +11,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -87,6 +90,41 @@ public class MVCController {
         model.addAttribute("current_user", userDetails.getUsername());
 
         return "user/study_interface";
+    }
+
+
+    /* Show the All vocabularies in edit_vocab.html*/
+    @GetMapping("user/editvoc")
+    public String getUserEditVocabulary(Model model) {
+
+        // tell the thymeleaf which user is logged in
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        // get the Query to show current vocabulary entries of user x
+        model.addAttribute("overview", this.vocabularyRepository.showAllVocabularyFromUserX(userDetails.getUsername()));
+
+        return "user/edit_vocab";
+    }
+
+
+    // user/editvoc/edit+${entry.id}}
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("edit_product");
+
+        VocabularyEntries vocabularyEntries = this.vocabularyRepository.getEntryBasedOnId(id);
+
+
+        System.out.println(vocabularyEntries.getId());
+
+
+        //        Product product = service.get(id);
+        //mav.addObject("product", product);
+
+
+
+        return mav;
     }
 
     /* Show the Form and Let the user enter stuff */
